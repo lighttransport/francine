dd48911ac61d2857948e344fc0bd4922f19eae3e
peryaudo <peryaudo@gmail.com>
2015-02-07 19:15:06 +0900

    Added incomplete GCE support

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt-get install -y nodejs npm supervisor
+update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+npm install
+
+cat > /etc/supervisor/conf.d/francine.conf <<EOF
+[program:francine]
+command=node /home/peryaudo/lib/main --instanceType=$1 --useMetadata=true
+autostart=true
+autorestart=true
+stderr_logfile = /var/log/supervisor/francine-stderr.log
+stdout_logfile = /var/log/supervisor/francine-stdout.log
+EOF
