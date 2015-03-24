9aaf48b002d9838d1f739f922119401a178aba85
peryaudo <peryaudo@gmail.com>
2015-03-24 11:28:33 +0900

    Fixed setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 apt-get update -y
-apt-get install -y nodejs npm supervisor
+apt-get install -y nodejs npm supervisor rsync
 update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
 npm install
 gcc ao.c lodepng.c -lm -o ao 
 
+sudo rsync -rtv `pwd` /root
+
 cat > /etc/supervisor/conf.d/francine.conf <<EOF
 [program:francine]
-command=node /home/peryaudo/lib/main --instanceType=$1 --useMetadata=true
+command=node /root/lib/main --instanceType=$1 --useMetadata=true
 autostart=true
 autorestart=true
 stderr_logfile = /var/log/supervisor/francine-stderr.log
