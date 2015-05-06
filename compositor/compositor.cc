bbc5ce016215b15254bc0b9e24cea46e4a54509e
peryaudo <peryaudo@gmail.com>
2015-05-06 14:11:53 +0900

    Added EXR image reducing support

--- compositor/compositor.cc
+++ compositor/compositor.cc
@@ -19,11 +19,14 @@ enum ImageFileFormats {
   kFormatExr
 };
 
-int LoadPng(const std::string& file_name, std::vector<double>* image, int* width, int* height) {
+int LoadPng(const std::string& file_name,
+            std::vector<double>* image,
+            int* width, int* height) {
   std::vector<unsigned char> decoded_image;
   unsigned unsigned_with, unsigned_height;
 
-  unsigned error = lodepng::decode(decoded_image, unsigned_with, unsigned_height, file_name);
+  unsigned error = lodepng::decode(decoded_image,
+                                   unsigned_with, unsigned_height, file_name);
   if (error) {
     fprintf(stderr, "failed to decode png image\n");
     return 1;
@@ -39,11 +42,14 @@ int LoadPng(const std::string& file_name, std::vector<double>* image, int* width
   return 0;
 }
 
-int LoadJpg(const std::string& file_name, std::vector<double>* image, int* width, int* height) {
+int LoadJpg(const std::string& file_name,
+            std::vector<double>* image,
+            int* width, int* height) {
   int actual_comps;
 
   unsigned char* decoded_image =
-    jpgd::decompress_jpeg_image_from_file(file_name.c_str(), width, height, &actual_comps, 4);
+    jpgd::decompress_jpeg_image_from_file(file_name.c_str(),
+                                          width, height, &actual_comps, 4);
 
   if (decoded_image == NULL) {
     fprintf(stderr, "failed to decode JPEG image\n");
@@ -57,7 +63,9 @@ int LoadJpg(const std::string& file_name, std::vector<double>* image, int* width
   return 0;
 }
 
-int LoadExr(const std::string& file_name, std::vector<double>* image, int* width, int* height) {
+int LoadExr(const std::string& file_name,
+            std::vector<double>* image,
+            int* width, int* height) {
   float* out_rgba;
   const char* error;
 
@@ -106,10 +114,14 @@ ImageFileFormats ReadFileSignature(const std::string& file_name) {
   return result;
 }
 
-int LoadImage(const std::string& file_name, std::vector<double>* image, int* width, int* height) {
+int LoadImage(const std::string& file_name,
+              std::vector<double>* image,
+              int* width, int* height) {
   switch (ReadFileSignature(file_name)) {
     case kFormatError:
-      fprintf(stderr, "failed to get the format of the input file %s", file_name.c_str());
+      fprintf(stderr,
+              "failed to get the format of the input file %s",
+              file_name.c_str());
       return 1;
     case kFormatPng:
       return LoadPng(file_name, image, width, height);
@@ -120,8 +132,15 @@ int LoadImage(const std::string& file_name, std::vector<double>* image, int* wid
   }
 }
 
-int SavePng(const std::string& file_name, const std::vector<unsigned char>& image, int width, int height) {
-  unsigned error = lodepng::encode(file_name, image, width, height);
+int SavePng(const std::string& file_name,
+            const std::vector<double>& image,
+            int width, int height) {
+  std::vector<unsigned char> output_image(image.size());
+  for (int i = 0; i < image.size(); ++i) {
+    output_image[i] = image[i];
+  }
+
+  unsigned error = lodepng::encode(file_name, output_image, width, height);
   if (error) {
     fprintf(stderr, "failed to encode png image\n");
     return 1;
@@ -129,29 +148,77 @@ int SavePng(const std::string& file_name, const std::vector<unsigned char>& imag
   return 0;
 }
 
-int SaveJpg(const std::string& file_name, const std::vector<unsigned char>& image, int width, int height) {
+int SaveJpg(const std::string& file_name,
+            const std::vector<double>& image,
+            int width, int height) {
   std::vector<unsigned char> three_channel_image(image.size() / 4 * 3);
   for (int i = 0, i_max = image.size() / 4; i < i_max; ++i) {
     three_channel_image[3 * i + 0] = image[4 * i + 0];
     three_channel_image[3 * i + 1] = image[4 * i + 1];
     three_channel_image[3 * i + 2] = image[4 * i + 2];
   }
-  jpge::compress_image_to_jpeg_file(file_name.c_str(), width, height, 3, &three_channel_image[0]);
+  jpge::compress_image_to_jpeg_file(file_name.c_str(),
+                                    width, height, 3,
+                                    &three_channel_image[0]);
   return 0;
 }
 
-int SaveExr(const std::string& file_name, const std::vector<unsigned char>& image, int width, int height) {
-  assert(false);
+int SaveExr(const std::string& file_name,
+            const std::vector<double>& image,
+            int width, int height) {
+  EXRImage exr;
+
+  exr.num_channels = 4;
+
+  const char *channel_names[] = {"R", "G", "B", "A"};
+  exr.channel_names = channel_names;
+
+  int pixel_types[] = {
+    TINYEXR_PIXELTYPE_FLOAT,
+    TINYEXR_PIXELTYPE_FLOAT,
+    TINYEXR_PIXELTYPE_FLOAT,
+    TINYEXR_PIXELTYPE_FLOAT};
+  exr.pixel_types = pixel_types;
+
+  exr.width = width;
+  exr.height = height;
+
+  std::vector<std::vector<float> > output_image(
+      4,
+      std::vector<float>(2 * width * height));
+
+  for (int i = 0, i_max = image.size() / 4; i < i_max; ++i) {
+    output_image[0][i] = image[4 * i + 0];
+    output_image[1][i] = image[4 * i + 1];
+    output_image[2][i] = image[4 * i + 2];
+    output_image[3][i] = image[4 * i + 3];
+  }
+
+  unsigned char *images[4];
+  for (int i = 0; i < 4; ++i) {
+    images[i] = reinterpret_cast<unsigned char*>(&output_image[i]);
+  }
+
+  exr.images = images;
+
+  const char *error;
+  if (SaveMultiChannelEXRToFile(&exr, file_name.c_str(), &error)) {
+    fprintf(stderr, "failed to encode EXR image: %s\n", error);
+    return 1;
+  }
+
+  return 0;
 }
 
 }  // namespace
 
 int main(int argc, char *argv[]) {
-	if (argc < 4) {
-		fprintf(stderr, "usage: compositor <output type> <output file> <input files> ...\n");
-		fprintf(stderr, "<output type> = png | jpg | exr\n");
-		return 1;
-	}
+  if (argc < 4) {
+    fprintf(stderr,
+            "usage: compositor <output type> <output file> <input files> ...\n");
+    fprintf(stderr, "<output type> = png | jpg | exr\n");
+    return 1;
+  }
 
   std::string output_type = argv[1];
   std::string output_file_name = argv[2];
@@ -190,19 +257,14 @@ int main(int argc, char *argv[]) {
     accumulated[i] /= count;
   }
 
-  std::vector<unsigned char> output_image(accumulated.size());
-  for (int i = 0; i < accumulated.size(); ++i) {
-    output_image[i] = accumulated[i];
+  if (output_type == "png") {
+    return SavePng(output_file_name, accumulated, width, height);
+  } else if (output_type == "jpg") {
+    return SaveJpg(output_file_name, accumulated, width, height);
+  } else if (output_type == "exr") {
+    return SaveExr(output_file_name, accumulated, width, height);
+  } else {
+    fprintf(stderr, "unsupported output file format %s\n", output_type.c_str());
+    return 1;
   }
-
-	if (output_type == "png") {
-    return SavePng(output_file_name, output_image, width, height);
-	} else if (output_type == "jpg") {
-    return SaveJpg(output_file_name, output_image, width, height);
-	} else if (output_type == "exr") {
-    return SaveExr(output_file_name, output_image, width, height);
-	} else {
-		fprintf(stderr, "unsupported output file format %s\n", output_type.c_str());
-		return 1;
-	}
 }
