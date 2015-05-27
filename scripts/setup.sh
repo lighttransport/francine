9615355187fff6d8a1ca17b62d4e7e54dfcae99b
peryaudo <peryaudo@gmail.com>
2015-05-27 15:59:34 +0900

    Fixed setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@
 set -e
 
 apt-get update -y
-apt-get install -y nodejs npm supervisor rsync build-essential g++ libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1 iptables-persistent
+apt-get install -y nodejs npm supervisor rsync build-essential g++ libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1
 update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
 npm install
 
@@ -15,7 +15,9 @@ gcc ao.c lodepng.c -lm -o ao
 # Configure NAT gateway
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-yes | service iptables-persistent save
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+apt-get install -y iptables-persistent
 
 # Compile Mallie renderer if exists
 if [ -d 'mallie' ]; then
